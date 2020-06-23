92a3715c3c6c8802dd1c2a69e4617a711715ebe7
Ruben Vorderman <r.h.p.vorderman@lumc.nl>
2020-06-23 10:42:31 +0200

    Simplify further by using system java

--- recipe/cromwell.py
+++ recipe/cromwell.py
@@ -21,27 +21,6 @@ PKG_NAME = 'cromwell'
 DEFAULT_JVM_MEM_OPTS = ('-Xms512m', '-Xmx1g')
 
 
-def jar_dir_from_bin_file(in_path: Path) -> Path:
-    """Returns the path to the JAR file"""
-    full_path = in_path.resolve()  # x/x/bin/my_script
-    # go to x/x/share/PKG_name
-    jar_path = full_path.parent.parent / "share" / PKG_NAME
-    return jar_path
-
-
-def java_executable() -> Path:
-    """Returns the name of the Java executable."""
-    java_home = os.getenv('JAVA_HOME')
-    java_bin = Path('bin', 'java')
-    env_prefix = jar_dir_from_bin_file(Path(sys.argv[0])).parent.parent
-
-    if java_home and os.access(str(Path(java_home, java_bin)), os.X_OK):
-        return Path(java_home, java_bin)
-    else:
-        # Use Java installed with Anaconda to ensure correct version
-        return env_prefix / 'bin' / 'java'
-
-
 def jvm_opts(argv, default_mem_opts=DEFAULT_JVM_MEM_OPTS):
     """Constructs a list of Java arguments based on our argument list.
 
@@ -69,8 +48,10 @@ def jvm_opts(argv, default_mem_opts=DEFAULT_JVM_MEM_OPTS):
 
 
 def main():
-    java = java_executable()
-    jar_dir = jar_dir_from_bin_file(Path(sys.argv[0]))
+    script = Path(sys.argv[0])
+    prefix = script.parent.parent      # Script is in prefix/bin/script.
+    jar_path = Path(prefix, "share", PKG_NAME, JAR_NAME)
+
     (mem_opts, prop_opts, pass_args) = jvm_opts(sys.argv[1:])
 
     if pass_args != [] and pass_args[0].startswith('org'):
@@ -78,8 +59,7 @@ def main():
     else:
         jar_arg = '-jar'
 
-    jar_path = jar_dir / JAR_NAME
-    java_args: List[str] = ([java] + mem_opts + prop_opts +
+    java_args: List[str] = (["java"] + mem_opts + prop_opts +
                             [jar_arg] + [str(jar_path)] + pass_args)
     sys.exit(subprocess.call(java_args))
 
